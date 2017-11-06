fce0382d6ab9317dc59bde8411d89002b5320348
Alfath Dioni <fathalfath30@gmail.com>
2017-11-06 19:25:52 +0700

    Class untuk keamanan data

--- SupplyChainManagement_S1/MainClass/Cls_Security.cs
+++ SupplyChainManagement_S1/MainClass/Cls_Security.cs
@@ -1,11 +1,66 @@
+/*
+ * Cls_Security
+ * 
+ * MD5 Hash script by Microsoft
+ */
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace SupplyChainManagement_S1
 {
-    public class Cls_Security
+    public static class Cls_Security
     {
+
+        private static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        // Verify a hash against a string.
+        static bool VerifyMd5Hash(MD5 md5Hash, string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = GetMd5Hash(md5Hash, input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static string GetMD5(string text)
+        {
+            string hash = "";
+            using (MD5 md5Hash = MD5.Create())
+            {
+                hash = GetMd5Hash(md5Hash, text);
+            }
+            return hash;
+        }
+
     }
 }
