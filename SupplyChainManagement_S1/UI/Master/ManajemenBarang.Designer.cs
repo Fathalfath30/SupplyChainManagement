d605ef3d414cc370ae403ddd7eb4c15d773738ac
Alfath Dioni <fathalfath30@gmail.com>
2017-11-07 19:12:44 +0700

    Perpindahan ManajemenBarang ke directory Master

---
+++ SupplyChainManagement_S1/UI/Master/ManajemenBarang.Designer.cs
@@ -0,0 +1,47 @@
+namespace SupplyChainManagement_S1.UI.Master
+{
+    partial class ManajemenBarang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ManajemenBarang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 491);
+            this.Name = "ManajemenBarang";
+            this.Text = "Manajemen Barang";
+            this.Load += new System.EventHandler(this.ManajemenBarang_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ SupplyChainManagement_S1/UI/Master/ManajemenBarang.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+namespace SupplyChainManagement_S1.UI.Master
+{
+    public partial class ManajemenBarang : MetroForm
+    {
+        public ManajemenBarang()
+        {
+            InitializeComponent();
+        }
+
+        private void ManajemenBarang_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
