e8dd304154aef14eb445a11a410d761c151c073b
Alfath Dioni <fathalfath30@gmail.com>
2017-11-22 08:55:48 +0700

    Penambahan form distributor

---
+++ SupplyChainManagement_S1/UI/Manufaktur/Frm_Distributor.Designer.cs
@@ -0,0 +1,47 @@
+namespace SupplyChainManagement_S1.UI.Manufaktur
+{
+    partial class Frm_Distributor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Frm_Distributor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Frm_Distributor";
+            this.Text = "Frm_Distributor";
+            this.Load += new System.EventHandler(this.Frm_Distributor_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
