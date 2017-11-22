2658fa8d820f0a006bef6dcf3bfd330ea198b0e9
Alfath Dioni <fathalfath30@gmail.com>
2017-11-22 08:49:14 +0700

    Update fungsi dashboard

--- SupplyChainManagement_S1/UI/Manufaktur/Frm_Dashboard.cs
+++ SupplyChainManagement_S1/UI/Manufaktur/Frm_Dashboard.cs
@@ -5,49 +5,66 @@ using MetroFramework.Forms;
 using SupplyChainManagement_S1.MainClass;
 using System.Globalization;
 using System.Threading;
+using System.ComponentModel;
 
 namespace SupplyChainManagement_S1.UI.Manufaktur
 {
     public partial class Frm_Dashboard : MetroForm
     {
-        private App_Data appData;
-        private DataSet MainDS;
-        private DataTable MainDT;
+        private App_Data appData = new App_Data();
         /* ----- [ MAIN SCRIPT ] ----- */
-        private void initDS()
+        private void InitGrid_Supplier()
         {
-            MainDS = new DataSet();
-            int rIndex = 0;
+            Grid_Supplier.Columns.Add("NamaSupplier", "Nama Supplier");
+            Grid_Supplier.Columns.Add("NamaBarang", "Nama Barang");
+            Grid_Supplier.Columns.Add("HargaBarang", "Harga Barang");
+            Grid_Supplier.Columns.Add("StockTersedia", "Stock Tersedia");
+            Grid_Supplier.Columns.Add("TipeStock", "Tipe Stock");
+            Grid_Supplier.Columns["HargaBarang"].DefaultCellStyle.Format = "c2";
 
-            // Data Supplier
-            MainDT = new DataTable("supplier");
-            MainDT.Columns.Add("NamaSupplier", typeof(string));
-            MainDT.Columns.Add("NamaBarang", typeof(string));
-            MainDT.Columns.Add("HargaBarang", typeof(string));
-            MainDT.Columns.Add("StockTersedia", typeof(string));
-
-            for (rIndex = 0; rIndex < appData.Data_Detil_Supplier.GetLength(0); rIndex ++)
+            Grid_Supplier.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            Grid_Supplier.ReadOnly = true;
+            Grid_Supplier.AutoGenerateColumns = false;
+            Grid_Supplier.AllowDrop = false;
+            Grid_Supplier.AllowUserToAddRows = false;
+            Grid_Supplier.AllowUserToDeleteRows = false;
+            for (int i = 0; i < Grid_Supplier.Columns.Count - 1; i++)
+                Grid_Supplier.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+        }
+        private void BindGrid_Supplier()
+        {
+            Grid_Supplier.Rows.Clear();
+            for (int rIndex = 0; rIndex < appData.Data_Detil_Supplier.GetLength(0); rIndex++)
             {
-                MainDT.Rows.Add(
+                Grid_Supplier.Rows.Add(
                     appData.Data_Detil_Supplier[rIndex, 1].ToString(),
                     appData.Data_Detil_Supplier[rIndex, 2].ToString(),
-                    appData.Data_Detil_Supplier[rIndex, 5].ToString(),
-                    appData.Data_Detil_Supplier[rIndex, 4].ToString()
+                    Convert.ToInt32(appData.Data_Detil_Supplier[rIndex, 5]),
+                    appData.Data_Detil_Supplier[rIndex, 4].ToString(),
+                    appData.Data_Detil_Supplier[rIndex, 2].ToString()
                     );
             }
-            MainDS.Tables.Add(MainDT);
-
-            // Data Produksi
-            MainDT = new DataTable("produksi");
-            MainDT.Columns.Add("KodeProduksi", typeof(string));
-            MainDT.Columns.Add("NamaManufaktur", typeof(string));
-            MainDT.Columns.Add("NamaBarang", typeof(string));
-            MainDT.Columns.Add("TanggalProduksi", typeof(string));
-            MainDT.Columns.Add("JumlahProduksi", typeof(string));
-            
-            for (rIndex = 0; rIndex < appData.Data_Produksi.GetLength(0); rIndex ++)
+        }
+        private void initGrid_Produksi() {
+            Grid_Produksi.Columns.Add("KodeProduksi", "Kode Produksi");
+            Grid_Produksi.Columns.Add("NamaManufaktur", "Nama Manufaktur");
+            Grid_Produksi.Columns.Add("NamaBarang", "Nama Barang");
+            Grid_Produksi.Columns.Add("TanggalProduksi", "Tanggal Produksi");
+            Grid_Produksi.Columns.Add("JumlahProduksi", "Jumlah Produksi");
+            Grid_Produksi.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            Grid_Produksi.ReadOnly = true;
+            Grid_Produksi.AutoGenerateColumns = false;
+            Grid_Produksi.AllowDrop = false;
+            Grid_Produksi.AllowUserToAddRows = false;
+            Grid_Produksi.AllowUserToDeleteRows = false;
+            for (int i = 0; i < Grid_Supplier.Columns.Count - 1; i++)
+                Grid_Supplier.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+        }
+        private void BindGrid_Produksi()
+        {
+            for (int rIndex = 0; rIndex < appData.Data_Produksi.GetLength(0); rIndex++)
             {
-                MainDT.Rows.Add(
+                Grid_Produksi.Rows.Add(
                     appData.Data_Produksi[rIndex, 0].ToString(),
                     appData.Data_Produksi[rIndex, 2].ToString(),
                     appData.Data_Produksi[rIndex, 3].ToString(),
@@ -55,11 +72,12 @@ namespace SupplyChainManagement_S1.UI.Manufaktur
                     appData.Data_Produksi[rIndex, 5].ToString()
                     );
             }
-            MainDS.Tables.Add(MainDT);
         }
-        /// <summary>
-        /// Method untuk menampilkan waktu dan tanggal di aplikasi
-        /// </summary>
+        private void InitComboBox()
+        {
+            Cmb_Filter_Produksi.SelectedIndex = 0;
+            Cmb_Filter_Supplier.SelectedIndex = 0;
+        }
         private void RefreshDateTime()
         {
             MLabel_Jam.Text = DateTime.Now.ToString("HH:mm:ss");
@@ -70,56 +88,20 @@ namespace SupplyChainManagement_S1.UI.Manufaktur
 
             MLabel_Tanggal.Text = dTime.ToString("dddd, dd MMMM yyyy");
         }
-        /// <summary>
-        /// Method untuk merefresh jumlah data yang ada dan ditampilkan di
-        /// MetroTile
-        /// </summary>
         private void RefreshMetroTile()
         {
-            MTile_Barang.TileCount = appData.Data_Barang.GetLength(0);
-            MTile_Barang.Refresh();
-
             MTile_Supplier.TileCount = appData.Data_Supplier.GetLength(0);
             MTile_Supplier.Refresh();
 
-            
+
             MTile_BarangJadi.TileCount = appData.Data_Produksi.GetLength(0);
             MTile_BarangJadi.Refresh();
-            
+
             MTile_SPBahanBaku.TileCount = appData.Data_SpBahanBaku.GetLength(0);
             MTile_SPBahanBaku.Refresh();
-            
+
             MTile_Distributor.TileCount = appData.Data_Distributor.GetLength(0);
             MTile_Distributor.Refresh();
-        }
-
-        private void BindDT_Supplier()
-        {
-            Gview_Supplier.DataSource = MainDS.Tables["supplier"];
-            Gview_Supplier.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            Gview_Supplier.ReadOnly = true;
-            Gview_Supplier.AutoGenerateColumns = false;
-            Gview_Supplier.AllowDrop = false;
-            Gview_Supplier.AllowUserToAddRows = false;
-            Gview_Supplier.AllowUserToDeleteRows = false;
-            for (int i = 0; i < Gview_Supplier.Columns.Count - 1; i++)
-                Gview_Supplier.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
-        }
-        /// <summary>
-        /// Melakukan inisialisasi untuk Data Grid View (Gview_Produksi) untuk 
-        /// menambahkan kolom.
-        /// </summary>
-        private void BindDT_Produksi()
-        {
-            Gview_Produksi.DataSource = MainDS.Tables["produksi"];
-            Gview_Produksi.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            Gview_Produksi.ReadOnly = true;
-            Gview_Produksi.AutoGenerateColumns = false;
-            Gview_Produksi.AllowDrop = false;
-            Gview_Produksi.AllowUserToAddRows = false;
-            Gview_Produksi.AllowUserToDeleteRows = false;
-            for (int i = 0; i < Gview_Produksi.Columns.Count - 1; i++)
-                Gview_Produksi.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
         }
         /* ----- [ GENERATED SCRIPT ] ----- */
         public Frm_Dashboard()
@@ -129,25 +111,39 @@ namespace SupplyChainManagement_S1.UI.Manufaktur
 
         private void Frm_Dashboard_Load(object sender, EventArgs e)
         {
-            appData = new App_Data();
+            InitGrid_Supplier();
+            BindGrid_Supplier();
 
-            CultureInfo culture = new CultureInfo("id-ID");
-            Thread.CurrentThread.CurrentCulture = culture;
+            initGrid_Produksi();
+            BindGrid_Produksi();
 
-            // Menginisialisasi Data Set
-            initDS();
-
-            // Memasukan DataSet
-            BindDT_Produksi();
-            BindDT_Supplier();
+            InitComboBox();
 
             Tmr_RefreshDT.Start();
             Tmr_Refresh_data.Start();
+
+            CultureInfo culture = new CultureInfo("id-ID");
+            Thread.CurrentThread.CurrentCulture = culture;
         }
 
-        private void Tmr_RefreshDT_Tick(object sender, EventArgs e)
+        private void Grid_Supplier_SortCompare(object sender, DataGridViewSortCompareEventArgs e)
         {
-            RefreshDateTime();
+            if (e.Column.Index == 2)
+            {
+                if (double.Parse(e.CellValue1.ToString()) > double.Parse(e.CellValue2.ToString()))
+                {
+                    e.SortResult = 1;
+                }
+                else if (double.Parse(e.CellValue1.ToString()) < double.Parse(e.CellValue2.ToString()))
+                {
+                    e.SortResult = -1;
+                }
+                else
+                {
+                    e.SortResult = 0;
+                }
+                e.Handled = true;
+            }
         }
 
         private void Tmr_Refresh_data_Tick(object sender, EventArgs e)
@@ -155,19 +151,127 @@ namespace SupplyChainManagement_S1.UI.Manufaktur
             RefreshMetroTile();
         }
 
-        private void Btn_Logout_Click(object sender, EventArgs e)
+        private void Tmr_RefreshDT_Tick(object sender, EventArgs e)
         {
-            DialogResult DResult = MessageBox.Show(
-                this,
-                "Apakah anda yakin ingin logout dari sistem ?",
-                "Konfirmasi",
-                MessageBoxButtons.OKCancel,
-                MessageBoxIcon.Question);
-            if (DResult == DialogResult.OK)
+            RefreshDateTime();
+        }
+
+        private void Cmb_Filter_Supplier_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Grid_Supplier.Rows.Clear();
+            if (Cmb_Filter_Supplier.SelectedIndex == 0)
             {
-                Close();
-                new UI.Main.Frm_Login().Show();
+                BindGrid_Supplier();
+            } 
+            else
+            {
+                int rIndex;
+                switch (Cmb_Filter_Supplier.SelectedIndex)
+                {
+                    case 1:
+                        rIndex = 0;
+                        Grid_Supplier.Rows.Add(
+                            appData.Data_Detil_Supplier[rIndex, 1].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 5].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 4].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString()
+                            );
+
+                        rIndex = 1;
+                        Grid_Supplier.Rows.Add(
+                            appData.Data_Detil_Supplier[rIndex, 1].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 5].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 4].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString()
+                            );
+                        break;
+                    case 2:
+                        rIndex = 1;
+                        Grid_Supplier.Rows.Add(
+                            appData.Data_Detil_Supplier[rIndex, 1].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 5].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 4].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString()
+                            );
+
+                        rIndex = 2;
+                        Grid_Supplier.Rows.Add(
+                            appData.Data_Detil_Supplier[rIndex, 1].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 5].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 4].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString()
+                            );
+                        break;
+                    case 3:
+                        rIndex = 6;
+                        Grid_Supplier.Rows.Add(
+                            appData.Data_Detil_Supplier[rIndex, 1].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 5].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 4].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString()
+                            );
+
+                        rIndex = 7;
+                        Grid_Supplier.Rows.Add(
+                            appData.Data_Detil_Supplier[rIndex, 1].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 5].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 4].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString()
+                            );
+                        break;
+                    case 4:
+                        rIndex = 5;
+                        Grid_Supplier.Rows.Add(
+                            appData.Data_Detil_Supplier[rIndex, 1].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 5].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 4].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString()
+                            );
+                        break;
+                    case 5:
+                        rIndex = 4;
+                        Grid_Supplier.Rows.Add(
+                            appData.Data_Detil_Supplier[rIndex, 1].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 5].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 4].ToString(),
+                            appData.Data_Detil_Supplier[rIndex, 2].ToString()
+                            );
+                        break;
+                }
             }
+        }
+
+        private void Cmb_Filter_Produksi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Grid_Produksi.Rows.Clear();
+            if (Cmb_Filter_Produksi.SelectedIndex < 1)
+            {
+                BindGrid_Produksi();
+            }
+            else
+            {
+                int rIndex = Cmb_Filter_Produksi.SelectedIndex - 1;
+                Grid_Produksi.Rows.Add(
+                    appData.Data_Produksi[rIndex, 0].ToString(),
+                    appData.Data_Produksi[rIndex, 2].ToString(),
+                    appData.Data_Produksi[rIndex, 3].ToString(),
+                    appData.Data_Produksi[rIndex, 4].ToString(),
+                    appData.Data_Produksi[rIndex, 5].ToString()
+                );
+            }
+        }
+
+        private void MTile_Barang_Click(object sender, EventArgs e)
+        {
+            new Frm_SPBahanBaku().ShowDialog();
         }
     }
 }
